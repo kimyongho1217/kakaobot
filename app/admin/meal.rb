84c91e38475c07e6a822699c84be5de25d4adb58
JonghunYu <yuzong@naver.com>
2016-11-24 00:07:40 +0900

    added active admin files

--- app/admin/meal.rb
+++ app/admin/meal.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register KakaoUser do
+ActiveAdmin.register Meal do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
