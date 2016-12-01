778d2ec38e06c25a178ce7c2f457e44c3b7740bc
JonghunYu <yuzong@naver.com>
2016-12-01 14:06:18 +0900

    got rid of wit client concern from food_unit model

--- app/models/food_unit.rb
+++ app/models/food_unit.rb
@@ -1,5 +1,3 @@
 class FoodUnit < ActiveRecord::Base
-  include WitClient
   belongs_to :food
-
 end
--- spec/models/food_unit_spec.rb
+++ spec/models/food_unit_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe FoodUnit, type: :model do
-  it_behaves_like "a wit client" do
-    let(:wit_client_class) { create(:food_unit) }
-  end
 end
