84ece4c2dd78afb92b7b0e01beb5cf2a66a626f5
JonghunYu <yuzong@naver.com>
2016-12-06 15:49:41 +0900

    WIP

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -20,8 +20,45 @@ class MessageController < ApplicationController
         return context
       },
       eatFoods: -> (request) {
+        puts request
+        entities = serialize_entities(request['entities'])
+        context = request['context']
+        context.merge! entities
+        unless context['Food'] 
+          context['missingFood'] = true
+          return context
+        end
 
+        unless context['FoodUnit'] 
+          context['missingUnit'] = true
+          return context
+        end
+
+        unless context['number'] 
+          context['missingNumber'] = true
+          return context
+        end
+
+        food = Food.find_by(name: context['Food'])
+        unless food
+          context['missingFoodInfo'] = context['Food']
+          return context
+        end
+        food_unit = FoodUnit.find_or_create_by(name: context['FoodUnit'], food: food)
+        meal = Meal.create(kakao_user: @kakao_user)
+        if entities["datetime"]
+          entities["datetime"]
+          meal.ate_at = entities["datetime"]
+        end
+        return context
       }
     })
   end
+
+  def serialize_entities(entities)
+    entities.reduce({}) do |m, entity|
+      m.merge!(entity[0] => entity[1][0].has_key?('value') ? entity[1][0]['value'] : entity[1][0])
+    end
+  end
+
 end
--- app/models/food_unit.rb
+++ app/models/food_unit.rb
@@ -1,3 +1,12 @@
 class FoodUnit < ActiveRecord::Base
   belongs_to :food
+
+  def synonyms_raw
+    self.synonyms.join("\n")
+  end
+
+  def synonyms_raw=(values)
+    self.synonyms = []
+    self.synonyms = values.split("\r\n")
+  end
 end
