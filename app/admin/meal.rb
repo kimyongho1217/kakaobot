36aaa7ebf2d0c950b67d0e60def969e23316d07b
JonghunYu <yuzong@naver.com>
2016-12-17 01:49:03 +0900

    show food information in meal sectino of adin

--- app/admin/meal.rb
+++ app/admin/meal.rb
@@ -1,17 +1,36 @@
 ActiveAdmin.register Meal do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
+  show do
+    attributes_table do
+      row :id
+      row :kakao_user
+      row :total_calorie_consumption
+      row 'Meal Foods' do |n|
+        columns do
+          column do
+           b "Food"
+          end
+          column do
+            b "Count"
+          end
+          column do
+            b "Calorie Consumption"
+          end
+        end
+        meal.meal_foods.includes(:food, :food_unit).each do |meal_food|
+          columns do
+            column do
+              (meal_food.food.name rescue "음식정보가없습니다")
+            end
+            column do
+              "#{meal_food.count}#{meal_food.food_unit.name rescue '개'}"
+            end
+            column do
+              meal_food.calorie_consumption
+            end
+          end
+        end
+      end
+    end
+  end
 
 end
