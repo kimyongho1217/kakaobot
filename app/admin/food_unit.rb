9ac218601c6526cda3fc9261059cb7324dd2bc8d
kim yong ho <kyhwin2000@gmail.com>
2016-12-26 01:31:08 +0900

    delete synonym in food unit
    csv파일 업로드 지속 실패로 아예 삭제해버

--- app/admin/food_unit.rb
+++ app/admin/food_unit.rb
@@ -1,13 +1,11 @@
 ActiveAdmin.register FoodUnit do
   active_admin_import timestamps: true,
-    on_duplicate_key_update: { columns: [:name, :weight_per_unit, :synonyms_raw] },
+    #on_duplicate_key_update: { columns: [:name, :weight_per_unit, :synonyms_raw, :food_id] },
     before_batch_import: proc { |import|
       import.csv_lines.uniq! {|line| line.first }
     }
 
-
-
-  permit_params :name, :weight_per_unit, :synonyms_raw, :food_id
+  permit_params :name, :weight_per_unit, :food_id
   form do |f|
     f.inputs "Food" do
       f.input :food, member_label: :name
@@ -15,7 +13,7 @@ ActiveAdmin.register FoodUnit do
     f.inputs "Unit Details" do
       f.input :name
       f.input :weight_per_unit
-      f.input :synonyms_raw, as: :text
+      #f.input :synonyms_raw, as: :text
     end
     f.actions
   end
